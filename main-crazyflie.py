1fca7385f3e6f865d4a4565febb5d59a39e6ffe2
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-07-18 21:21:33 +0200

    added step 3 of running of tests

--- main-crazyflie.py
+++ main-crazyflie.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt # for plotting
+import random as rnd            # for setting the seed of the random generator
+from os.path import exists      # to check if test has already been performed
 
 # utilities of testing approach
 from ControlBasedTesting.binary_search_sinus_freq import binary_search_sinus_freq
@@ -9,6 +11,7 @@ from ControlBasedTesting.shapeTestSet import shapeTestSet
 from CrazyflieSimulationPython.cfSimulator import cfSimulation, ZAnalysis, zTest
 
 data_directory = 'cfdata/'
+rnd.seed(1)
 
 ### TODO: set seed for random test generation ###
 
@@ -54,21 +57,21 @@ print("Phase 1 done: I have sampled {} frequencies".format(sinusoidal_upper_boun
 # INPUTS: shapes, f_min, f_max, nl_th, num_tests per shape
 # OUTPUT: a vector of length num_tests (d,A) pairs for each shape
 
-num_tests = 100 # number of randomly generated tests
+num_tests = 2 # number of randomly generated tests per shape
 
 test_set = []
 
 # generate testset
 i=0
 for s in zTest.shapes :
-    if not(s=='sinus') :
+    if not(s=='sinus') : # we are not interested in sinus test cases at this step
         print('generating test set for shape: '+s)
         test_set.append(shapeTestSet(zTest,s,sinusoidal_upper_bound,0.001))
         test_set[i].generate_test_set(num_tests)
-        test_set[i].plot_test_set()
+        # test_set[i].plot_test_set()
         i=i+1
 
-plt.show()
+# plt.show()
 
 ########################################################
 ### PHASE 3: Tests Execution & check of MRs on tests ###
@@ -76,7 +79,21 @@ plt.show()
 # INPUTS: test set
 # OUTPUT: frequency-amplitude points and associated behaviour
 
-# run tests
+i=0
+for s in zTest.shapes :      ## iterate over shapes
+    if not(s=='sinus') :     # we do not want to run sinus test cases at this step
+        for test in test_set[i].test_cases : ## iterate over
+            #
+            print("Running test: Shape: {} Amp_scale: {} Time_scale: {}".format(s,test['a_gain'],test['t_scale']))
+            test_input = zTest(s,test['a_gain'],test['t_scale'])
+            file_path = data_directory+s+'-'+str(test['a_gain'])+'-'+str(test['t_scale'])
+            if not(exists(file_path)) :
+                sut    = cfSimulation()      # initialize simulation object
+                result = sut.run(test_input)       # test execution
+                result.save(name=file_path)
+            else :
+                print("test already executed: "+file_path)
+        i=i+1
 
 # check MRs
 
