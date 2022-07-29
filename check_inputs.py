8473cf274f63288366f94daa10427e7d861c33a0
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-07-29 17:39:48 +0200

    update script

--- check_inputs.py
+++ check_inputs.py
@@ -12,6 +12,10 @@ Mainly for avoiding having to re-run phases 1, 2, and 3 every time.
 ref = np.linspace(0,1,101)
 
 faCharact = faCharacterization.open(sys.argv[1])
+
+faCharact.plot_non_linearity_characterization()
+faCharact.plot_filtering_characterization(0.3 )
+
 faCharact.check_input(ref, 0.01)
 
 plt.show()
