5d94b27ea6fca48b065565d06782956f688d7fca
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-07-30 11:27:03 +0200

    fix script

--- check_inputs.py
+++ check_inputs.py
@@ -1,5 +1,6 @@
 import sys # for command line arguments
 import matplotlib.pyplot as plt # for plotting
+from CrazyflieSimulationPython.cfSimulator.zTest import zTest
 import numpy as np
 
 from ControlBasedTesting.faCharacterization import faCharacterization
@@ -9,10 +10,11 @@ Script that uses a stored characterization to check a given input on it.
 Mainly for avoiding having to re-run phases 1, 2, and 3 every time.
 '''
 
-ref = np.linspace(0,1,101)
-
+dt = 0.001
 faCharact = faCharacterization.open(sys.argv[1])
 
-faCharact.check_input(ref, 0.01, 0.3)
+ref = zTest('ud1',10.5, 0.2)
+
+faCharact.check_input(ref, dt, 0.3)
 
 plt.show()
