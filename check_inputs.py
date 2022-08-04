03562e695a2b8b86378e7c35a2b1a5e9cc5d8384
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-08-04 18:58:09 +0200

    use variable for non lin th

--- check_inputs.py
+++ check_inputs.py
@@ -14,6 +14,7 @@ Mainly for avoiding having to re-run phases 1, 2, and 3 every time.
 
 directory = "cfdata_nlmax015/"
 dt = 0.001
+non_linear_threshold = 0.15
 faCharact = faCharacterization.open(sys.argv[1])
 
 shape = 'ud1'
@@ -27,6 +28,7 @@ if exists(directory+file_name) :
 
 ref = zTest(shape,a_gain,t_scale)
 
-faCharact.plot_input_mapping_on_characterization(ref, dt, 0.3)
+faCharact.plot_amp_lower_bound(non_linear_threshold)
+faCharact.plot_input_mapping_on_characterization(ref, dt, non_linear_threshold)
 
 plt.show()
