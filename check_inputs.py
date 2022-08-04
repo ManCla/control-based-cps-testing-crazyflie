1f9aa2eb43b1cd50011f50bc239932fe6dcf7ed2
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-08-04 21:34:17 +0200

    minors

--- check_inputs.py
+++ check_inputs.py
@@ -12,23 +12,26 @@ Script that uses a stored characterization to check a given input on it.
 Mainly for avoiding having to re-run phases 1, 2, and 3 every time.
 '''
 
+try_plot_test = False # use if you want to try plotting also the actual execution of the test
+
 directory = "cfdata_nlmax015/"
 dt = 0.001
 non_linear_threshold = 0.15
 faCharact = faCharacterization.open(sys.argv[1])
 
-shape = 'ud1'
-a_gain = 4.6717
-t_scale = 0.4573714
+shape = 'sinus'
+a_gain = 1.5374999999999999
+t_scale = 0.574999988079071
 file_name = shape+'-'+str(a_gain)+'-'+str(t_scale)
-if exists(directory+file_name) :
+if try_plot_test and exists(directory+file_name) :
     test_results = fdh()
     test_results.open(directory+file_name)
     test_results.show_all()
 
 ref = zTest(shape,a_gain,t_scale)
 
-faCharact.plot_amp_lower_bound(non_linear_threshold)
+# faCharact.plot_amp_lower_bound(non_linear_threshold)
 faCharact.plot_input_mapping_on_characterization(ref, dt, non_linear_threshold)
+faCharact.check_input_on_characterization(ref, dt)
 
 plt.show()
