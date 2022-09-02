fe8f8e3c5d42a602c9c3d9e7c9ec9cd473a07e17
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-09-02 20:08:51 +0200

    small changes to script

--- plot-faCharact.py
+++ plot-faCharact.py
@@ -12,18 +12,23 @@ from command line and plots it
 
 nl_max = 0.15
 shape = 'none'
+shapes = ['steps', 'ramp', 'trapezoidal', 'triangular']
+linear_points_only = False
 
 faCharact = faCharacterization.open(sys.argv[1])
 
 faCharact.check_freq_ordering()
 
-for s in ['steps', 'ramp', 'trapezoidal', 'triangular'] :
-    faCharact.evaluate_shape_bandwidth(s,nl_max,show_plot=False)
+for s in shapes :
+    faCharact.evaluate_shape_bandwidth(s,nl_max,show_plot=True)
 
 # plot obtained characterization
 faCharact.plot_non_linearity_characterization(nl_max,shape=shape)
 faCharact.plot_filtering_characterization(nl_max,shape=shape)
 faCharact.plot_motors_saturation_characterization(shape=shape)
 faCharact.plot_hit_ground_characterization(shape=shape)
+
+# generate csv file with fa points
+# faCharact.generate_csv(shapes, nl_max, linear_points_only)
 
 plt.show()
